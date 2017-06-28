0b99c7985f6e85ef8e009617563703460c844199
Alexis - Real 4 Life <arosa@tpipr.com>
2017-06-28 15:21:45 -0400

    add template sections using bulma default

--- views/content/search.php
+++ views/content/search.php
@@ -1,1 +1,9 @@
-<p>content</p>
+<section class="section" style="height: 750px">
+    <div class="container">
+        <h1 class="title">Section</h1>
+        <h2 class="subtitle">
+            A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+        </h2>
+    </div>
+</section>
+
