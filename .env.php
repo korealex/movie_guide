f72c0c2fa3e9b0172d9f238a55b8a9cc8ab0632f
Alex <arosa@rallypages.com>
2017-07-06 19:07:50 -0400

    fix envs

---
+++ .env.php
@@ -0,0 +1,11 @@
+<?php
+putenv("TVDB_TOKEN=<token>");
+putenv("TVDB_API_KEY=<key>");
+putenv("TVDB_USER_KEY=<user_key>");
+putenv("TVDB_USERNAME=<username>");
+
+putenv("DB_USER=<db_user>");
+putenv("DB_PASS=<db_pass>");
+putenv("DB_NAME=<db_name>");
+putenv("DB_HOST=<host>");
+putenv("DB_PORT=<port>");
